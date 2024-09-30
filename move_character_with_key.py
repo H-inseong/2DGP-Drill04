0b4fb12cea9a0f562915d5b3edcaa2446f89d5ad
hwanginseong@TIP219 <3dlstjd3@gmail.com>
2024-09-30 15:03:04 +0900

    마우스 트래킹 구현

--- move_character_with_key.py
+++ move_character_with_key.py
@@ -8,7 +8,7 @@ character = load_image('animation_sheet.png')
 
 def handle_events():
     global running
-    global x
+    global dir
 
     events = get_events()
     for event in events:
@@ -16,16 +16,22 @@ def handle_events():
             running = False
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
-                x += 10
+                dir += 1
             elif event.key == SDLK_LEFT:
-                x -= 10
+                dir -= 1
             elif event.key == SDLK_ESCAPE:
                 running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir -= 1
+            elif event.key == SDLK_LEFT:
+                dir += 1
 
 
 running = True
 x = 800 // 2
 frame = 0
+dir = 0
 
 while running:
     clear_canvas()
@@ -34,6 +40,7 @@ while running:
     update_canvas()
     handle_events()
     frame = (frame + 1) % 8
+    x += dir * 5
     delay(0.05)
 
 close_canvas()
